c5db28c558d6b5c38b0e85d19e03db613d631b75
Kevlin <kevlin.susanto@yahoo.com>
2021-04-14 17:44:08 +0200

    update

--- pages/panier.vue
+++ pages/panier.vue
@@ -43,7 +43,7 @@
 
     export default {
         head: {
-            title: "Panier Vue JS",
+            title: "Panier",
             meta: [
                 {
                     hid: 'description',
